a22f27d8e76afade344fba330e8a21f72cca9f36
danik-miron0-ai <danik-miron0@yandex.ru>
2026-05-11 12:00:37 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,12 @@
 import telebot
 import requests
+from flask import Flask, request
 
 BOT_TOKEN = "8007635004:AAEEEhVhuPui8Seg9qBauyzFkTBiAU4ZbxE"
 GETCOURSE_KEY = "FjqP3bEpvsFB9xk53DcF4UJ9GA4TWG8VOpdKHWwezcVHBQm1ABDsB4s55mMQa0HSnXHsZfbA1bjT7yFm8ab3ZSlfM5zKmxI4uDqGI72mydAboqp9M6P6HcHyfGFFjgos"
 GETCOURSE_DOMAIN = "https://chinabusinessacademy.getcourse.ru"
-
 bot = telebot.TeleBot(BOT_TOKEN)
+app = Flask(__name__)
 
 @bot.message_handler(commands=['start'])
 def start(message):
@@ -28,4 +29,15 @@ def handle_text(message):
 
     bot.send_message(telegram_id, "Готово! Ты привязан.")
 
-bot.infinity_polling()
+@app.route("/", methods=["GET"])
+def home():
+    return "OK"
+
+@app.route(f"/{BOT_TOKEN}", methods=["POST"])
+def webhook():
+    update = telebot.types.Update.de_json(request.data.decode("utf-8"))
+    bot.process_new_updates([update])
+    return "OK"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
